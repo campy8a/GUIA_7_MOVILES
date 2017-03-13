29ef7366bea4c6d6e3de1f75c10374d63b5663c2
Felipe Campos <fa.campos10@uniandes.edu.co>
2017-03-13 10:25:40 -0500

    Camara implementada

--- app/src/main/java/com/dhamova/fvr/RegistroActivity.java
+++ app/src/main/java/com/dhamova/fvr/RegistroActivity.java
@@ -5,11 +5,14 @@ import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.provider.MediaStore.Images;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
@@ -23,6 +26,8 @@ import android.widget.ImageView;
 import android.widget.Toast;
 
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.URI;
 import java.util.jar.*;
 
@@ -57,31 +62,37 @@ public class RegistroActivity extends AppCompatActivity {
 
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data)
-    {
-        if(requestCode == REQUEST_PHOTO && resultCode == Activity.RESULT_OK)
-        {
-            if(data!=null)
-            {
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_PHOTO && resultCode == Activity.RESULT_OK) {
+            if (data != null) {
                 imagetoUploadUri1 = data.getData();
             }
-            if(imagetoUploadUri1!=null)
-            {
+            if (imagetoUploadUri1 != null) {
                 Uri selectedImage = imagetoUploadUri1;
-                getContentResolver().notifyChange(selectedImage,null);
+                getContentResolver().notifyChange(selectedImage, null);
 
-                if( ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)!= PackageManager.PERMISSION_GRANTED)
+                if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED)
                 {
-                    ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.WRITE_EXTERNAL_STORAGE},REQUEST_PERMISSION);
-                    Log.d(TAG,"No hay permiso");
-                }
-               // Bitmap reducedSizeBitmap = getBitmap(getRealPathFromUri(imagetoUploadUri1));
+                    ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_PERMISSION);
+                    Log.d(TAG, "No hay permiso");
 
+                    Bitmap reducedSizeBitmap = getBitmap(getRealPathFromURI(imagetoUploadUri1));
+                    if (reducedSizeBitmap != null) {
+                        Log.d(TAG, "llega bien");
+                        imagen.setImageBitmap(reducedSizeBitmap);
+                    }
+                }
+                else {
+                    Toast.makeText(this, "ERROR CAPTURANDO IMAGEN DE URI", Toast.LENGTH_LONG).show();
+                }
+
+
+                }
 
             }
-
         }
-    }
+
+
 
 
 
@@ -93,7 +104,7 @@ public class RegistroActivity extends AppCompatActivity {
             Toast.makeText(this,"Este dispositivo no tiene camara",Toast.LENGTH_SHORT).show();
             return;
         }
-        Intent pickIntent2 = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        Intent pickIntent2 = new Intent(Intent.ACTION_PICK, Images.Media.EXTERNAL_CONTENT_URI);
         pickIntent2.setType("image/*");
         Intent takePictureIntent= new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         File f = new File(Environment.getExternalStorageDirectory(),"/drawable/user.png");
@@ -109,4 +120,85 @@ public class RegistroActivity extends AppCompatActivity {
         }
     }
 
+
+    private Bitmap getBitmap(String path) {
+
+        Uri uri = Uri.fromFile(new File(path));
+        InputStream in = null;
+        try {
+            final int IMAGE_MAX_SIZE = 1200000; // 1.2MP
+            in = getContentResolver().openInputStream(uri);
+
+            // Decode image size
+            BitmapFactory.Options o = new BitmapFactory.Options();
+            o.inJustDecodeBounds = true;
+            BitmapFactory.decodeStream(in, null, o);
+            in.close();
+
+
+            int scale = 1;
+            while ((o.outWidth * o.outHeight) * (1 / Math.pow(scale, 2)) >
+                    IMAGE_MAX_SIZE) {
+                scale++;
+            }
+            Log.d("", "scale = " + scale + ", orig-width: " + o.outWidth + ", orig-height: " + o.outHeight);
+
+            Bitmap b = null;
+            in = getContentResolver().openInputStream(uri);
+            if (scale > 1) {
+                scale--;
+                // scale to max possible inSampleSize that still yields an image
+                // larger than target
+                o = new BitmapFactory.Options();
+                o.inSampleSize = scale;
+                b = BitmapFactory.decodeStream(in, null, o);
+
+                // resize to desired dimensions
+                int height = b.getHeight();
+                int width = b.getWidth();
+                Log.d("", "1th scale operation dimenions - width: " + width + ", height: " + height);
+
+                double y = Math.sqrt(IMAGE_MAX_SIZE
+                        / (((double) width) / height));
+                double x = (y / height) * width;
+
+                Bitmap scaledBitmap = Bitmap.createScaledBitmap(b, (int) x,
+                        (int) y, true);
+                b.recycle();
+                b = scaledBitmap;
+
+                System.gc();
+            } else {
+                b = BitmapFactory.decodeStream(in);
+            }
+            in.close();
+
+            Log.d("", "bitmap size - width: " + b.getWidth() + ", height: " +
+                    b.getHeight());
+            return b;
+        } catch (IOException e) {
+            Log.e("", e.getMessage(), e);
+            return null;
+        }
+    }
+    private String getRealPathFromURI(Uri contentURI) {
+        String result ="";
+        Cursor cursor = getContentResolver().query(contentURI, null, null, null, null);
+        if (cursor == null) { // Source is Dropbox or other similar local file path
+            result = contentURI.getPath();
+        } else {
+            cursor.moveToFirst();
+            try{
+                int idx = cursor.getColumnIndex(MediaStore.Images.ImageColumns.DATA);
+                result = cursor.getString(idx);
+
+            }catch(Exception e)
+            {
+                Log.d("data", contentURI.toString());
+                return contentURI.getPath();
+            }
+            cursor.close();
+        }
+        return result;
+    }
 }
